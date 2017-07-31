158cbe63ffb3b14cd5dabec8324ff1b56a0ade4e
deeptiboddapati <deeptiboddapati@gmail.com>
2017-07-31 03:14:37 -0500

    adding starter code and scratch requirements doc

---
+++ multisite-stat-counter.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Plugin Name:       Multisite Plugin Starter
+ * Plugin URI:        http://deeptiboddapati.com
+ * Description:       This adds a widget that displays the number of posts and users per site in your Multisite install.
+ * Version:           1.0
+ * Author:           Deepti Boddapati
+ * Author URI:        http://deeptiboddapati.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       multisite-stats
+ */
+
+namespace MULTISITE_STATS;
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+// The class that contains the plugin info.
+require_once plugin_dir_path( __FILE__ ) . 'includes/class-info.php';
+/**
+ * The code that runs during plugin activation.
+ */
+function activation() {
+	require_once plugin_dir_path( __FILE__ ) . 'includes/class-activator.php';
+	Activator::activate();
+}
+register_activation_hook( __FILE__, __NAMESPACE__ . '\\activation' );
+
+/**
+ * Run the plugin.
+ */
+// function run() {
+// 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-plugin.php';
+// 	$plugin = new Plugin();
+// 	$plugin->run();
+// }
+// run();
