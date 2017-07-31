e1544474ded81398b704c6549a895b3c1b2d64d3
deeptiboddapati <deeptiboddapati@gmail.com>
2017-07-31 06:21:29 -0500

    adding starter widget code

--- multisite-stat-counter.php
+++ multisite-stat-counter.php
@@ -136,3 +136,53 @@ add_action( 'rest_api_init', function () {
 		'callback' => 'MULTISITE_STATS\network_stats_endpoint',
 	) );
 } );
+
+
+class My_Widget extends \WP_Widget {
+
+	/**
+	 * Sets up the widgets name etc
+	 */
+	public function __construct() {
+		$widget_ops = array( 
+			'classname' => 'my_widget',
+			'description' => 'My Widget is awesome',
+		);
+		parent::__construct( 'my_widget', 'My Widget', $widget_ops );
+	}
+
+	/**
+	 * Outputs the content of the widget
+	 *
+	 * @param array $args
+	 * @param array $instance
+	 */
+	public function widget( $args, $instance ) {
+		echo " hi";
+		// outputs the content of the widget
+	}
+
+	/**
+	 * Outputs the options form on admin
+	 *
+	 * @param array $instance The widget options
+	 */
+	public function form( $instance ) {
+		// outputs the options form on admin
+	}
+
+	/**
+	 * Processing widget options on save
+	 *
+	 * @param array $new_instance The new options
+	 * @param array $old_instance The previous options
+	 */
+	public function update( $new_instance, $old_instance ) {
+		// processes widget options to be saved
+	}
+}
+
+add_action( 'widgets_init', function(){
+	register_widget( 'Multisite_Stats\My_Widget' );
+});
+
