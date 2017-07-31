658aa1cd8c1bdab71ecf6e567294ca0455c7d091
deeptiboddapati <deeptiboddapati@gmail.com>
2017-07-31 09:27:00 -0500

    Adding documentation to functions and classes

--- multisite-stat-counter.php
+++ multisite-stat-counter.php
@@ -9,6 +9,8 @@
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       multisite-stats
+ *
+ * @package MULTISITE_STATS\
  */
 
 namespace MULTISITE_STATS;
@@ -17,11 +19,36 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
+/**
+ * Enables Users Endpoint to show all users.
+ *
+ * Filtered the users query so that the endpoint shows unpublished and published users.
+ *
+ * @since 1.0
+ *
+ * @link https://github.com/WP-API/WP-API/issues/2300
+ */
 class UserFields {
-
+	/**
+	 * Filters users endpoint.
+	 *
+	 * Adds filter to Rest API Users endpoint that will
+	 * allow the endpoint to show all users not just published ones.
+	 *
+	 * @since 1.0
+	 */
 	function __construct() {
-		add_filter( 'rest_user_query',           [ $this, 'show_all_users' ] );
+		add_filter( 'rest_user_query', [ $this, 'show_all_users' ] );
 	}
+
+	/**
+	 * Removes the check for published posts.
+	 *
+	 * Unsets the 'has_published_posts' key fron the rest api endpoint args.
+	 *
+	 * @since 1.0
+	 * @param  array $prepared_args Arguments passed to the user query.
+	 */
 	function show_all_users( $prepared_args ) {
 		unset( $prepared_args['has_published_posts'] );
 		return $prepared_args;
@@ -29,19 +56,19 @@ class UserFields {
 }
 new UserFields();
 /**
- * Summary.
+ * Refreshes network wide statistics.
  *
- * Description.
+ * Refreshes statistics for the whole network. Sets them as a transient that expires hourly.
  *
  * @since 1.0
  */
-function get_network_stats() {
+function refresh_network_stats() {
 	$sites = get_sites();
 	$stats = array();
 	foreach ( $sites as $site ) {
 		$domain = $site->domain;
-		$site_users = get_site_user_count( $domain );
-		$site_posts = get_site_post_count( $domain );
+		$site_users = get_blog_user_count( $domain );
+		$site_posts = get_blog_post_count( $domain );
 		$site_stats = array();
 		$site_stats['domain'] = $domain;
 		$site_stats['site_users'] = $site_users;
@@ -55,20 +82,20 @@ if ( ! wp_next_scheduled( 'refresh_network_stats' ) ) {
 	wp_schedule_event( time(), 'hourly', 'refresh_network_stats' );
 }
 
-add_action( 'refresh_network_stats', 'MULTISITE_STATS\get_network_stats' );
+add_action( 'refresh_network_stats', 'MULTISITE_STATS\refresh_network_stats' );
 
 
 /**
- * Summary.
+ * Gets the number of users on a blog.
  *
- * Description.
+ * Gets the number of users on a single blog using the blog domain and the rest api.
  *
  * @since 1.0
  *
  * @param string $site_domain Description.
- * @return int $user_count Users in site.
+ * @return int $user_count Users in blog.
  */
-function get_site_user_count( $site_domain ) {
+function get_blog_user_count( $site_domain ) {
 	$user_count = 0;
 	$endpointurl = 'http://' . $site_domain . '/wp-json/wp/v2/users';
 	$response = wp_remote_get( $endpointurl );
@@ -77,41 +104,42 @@ function get_site_user_count( $site_domain ) {
 }
 
 /**
- * Summary.
+ * Gets the number of posts on a blog.
  *
- * Description.
+ * Gets the number of posts on a single blog using the blog domain and the rest api.
  *
  * @since 1.0
  *
  * @param string $site_domain Description.
- * @return int $post_count Posts in site.
+ * @return int $post_count Posts in blog.
  */
-function get_site_post_count( $site_domain ) {
+function get_blog_post_count( $site_domain ) {
 	$post_count = 0;
 	$endpointurl = 'http://' . $site_domain . '/wp-json/wp/v2/posts';
+	// The phpcs error/reccomendation to use vip_safe_remote_get() doesn't apply to my enviornment.
 	$response = wp_remote_get( $endpointurl );
 	$post_count = wp_remote_retrieve_header( $response, 'x-wp-total' );
 	return $post_count;
 }
-
-function network_stats_endpoint(){
+/**
+ * Returns an array of network stats.
+ *
+ * Checks to see if the transient for network stats is set.
+ * If it's not set it refreshes the network stats.
+ *
+ * @since 1.0
+ *
+ * @return array $stats An array of network stats.
+ */
+function network_stats_endpoint() {
 	$stats = get_site_transient( 'network_stats' );
-	if ( ! $stats ){
-		get_network_stats();
+	if ( ! $stats ) {
+		refresh_network_stats();
 		$stats = get_site_transient( 'network_stats' );
 	}
 	return $stats;
 }
-/**
- * Summary.
- *
- * Description.
- *
- * @since 1.0
- *
- * @param string $site_domain Description.
- * @return int $post_count Posts in site.
- */
+
 add_action( 'rest_api_init', function () {
 	register_rest_route( 'multisitestats/v1', '/stats/', array(
 		'methods' => 'GET',
@@ -119,7 +147,16 @@ add_action( 'rest_api_init', function () {
 	) );
 } );
 
-
+/**
+ * Adds Multisite Stats Widget.
+ *
+ * Declares the Multisite Stats widget. Fetches stats from the REST api
+ * and outputs in any widget area.
+ *
+ * @since 1.0
+ *
+ * @see \WP_Widget
+ */
 class MultiSiteStats extends \WP_Widget {
 
 	/**
@@ -127,16 +164,19 @@ class MultiSiteStats extends \WP_Widget {
 	 */
 	public function __construct() {
 		$widget_ops = array(
-			'classname' => 'my_widget',
-			'description' => 'My Widget is awesome',
+			'classname' => 'multisite_stats',
+			'description' => 'Displays domain, number of users, and number of posts for each blog in the Multisite.',
 		);
-		parent::__construct( 'my_widget', 'My Widget', $widget_ops );
+		parent::__construct( 'multisite_stats', 'Multisite Stats', $widget_ops );
 	}
 
 	/**
 	 * Outputs the content of the widget
+	 *
+	 * @param  array  $args Widget arguments to match the base class.
+	 * @param  object $instance Widget instance to match the base class.
 	 */
-	public function widget() {
+	public function widget( $args, $instance ) {
 		$endpointurl = get_site_url( ) . '/wp-json/multisitestats/v1/stats/';
 		$response = wp_remote_get( $endpointurl );
 		$body = wp_remote_retrieve_body( $response );
